e15ed283327a8dffdbdf7b9f97f5b125664b3be5
Vitor Dias dos Santos <vitordias09532@gmail.com>
2025-03-10 10:28:02 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,14 @@
+x = 256
+y = x - 128
+print(x)
+print(y)
+print(x + y)
+print(x * y)
+print(x / y)
+print(x ** y) # exponênciação
+print(x % y)
+
+texto1 = 'Ola'
+print(texto1)
+texto2 = 'como vai voce?'
+print(texto1 + ', ' + texto2)
